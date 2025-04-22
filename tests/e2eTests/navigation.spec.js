29db3f926c9687fcf6d83186e817530c34a4d97f
HelChris <111705707+HelChris@users.noreply.github.com>
2025-04-22 21:59:48 +0200

    add navigation.spec.js

--- tests/e2eTests/navigation.spec.js
+++ tests/e2eTests/navigation.spec.js
@@ -2,20 +2,16 @@ import { test, expect } from "@playwright/test";
 
 test.describe("navigation", () => {
   test("navigate from home page to venue details", async ({ page }) => {
-    //nav to homepage:
-    await page.goto("/index.html");
+    // Navigate to homepage:
+    await page.goto("/");
 
-    //wait for venue list to load
+    // Wait for venue list to load
     await page.locator("#venue-container").waitFor();
 
-    // clicks the first venue
-    await page.locator("#venue-container a").first().click();
+    // Click the first venue
+    await page.click("#venue-container a:first-child");
 
-    //wait for venue details page to load
-    await page.waitForURL("**/venue/**");
-    //verifies the words "venue details" is in the heading
-    await expect(
-      page.getByRole("heading", { name: "Venue details" }),
-    ).toBeVisible();
+    // Verify venue details page heading
+    await expect(page.locator("h1")).toContainText("Venue details");
   });
 });
